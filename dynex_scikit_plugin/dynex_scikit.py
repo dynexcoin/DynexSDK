239da48462fc296f81caac46a019c78efd60bb4c
Dynex [DNX] <113523376+dynexcoin@users.noreply.github.com>
2023-11-08 08:53:21 +0100

    Compatibility fixes to 0.1.10

--- dynex_scikit_plugin/dynex_scikit.py
+++ dynex_scikit_plugin/dynex_scikit.py
@@ -269,12 +269,12 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
         # run on Dynex platform:
         bqm, invert = dimod.cqm_to_bqm(cqm) # Convert CQM->BQM
         model = dynex.BQM(bqm);
-        sampler = dynex.DynexSampler(model, mainnet=False, description='Dynex SciKit Plugin');
+        sampler = dynex.DynexSampler(model, mainnet=True, description='Dynex SciKit Plugin');
         sampleset_x = sampler.sample(num_reads=2048, annealing_time = 200, debugging=False);
         sampleset = invert(sampleset_x.first.sample);
         lowest = sampleset_x.first.sample;
         feasible = cqm.check_feasible(sampleset, atol=0.1);
-        print('Best energy:', sampler.dimod_assignments.first.energy,"feasible?", feasible);
+        print('Best energy:', sampleset_x.first.energy,"feasible?", feasible);
 
 
         self._mask = np.fromiter((lowest[v] for v in cqm.variables),
